d8799fe457c51f9554647b85d8eb6edbeb0e85c2
Nitish Kumar <krnitish@live.in>
2021-05-24 22:46:57 +0530

    React blog page configured

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -14,8 +14,8 @@ app.post('/posts/:id.comments', (req, res) => {
     const commentId = randomBytes(4).toString('hex')
     const { content } = req.body
     const comments = commentsByPostId[req.params.id] || []
-    comments.push({ id: commentId, content })
-    commentsByPostId[req.params.id] = comments
+    comments.push({ id: commentId, content });
+    commentsByPostId[req.params.id] = comments;
 
     res.status(201).send(comments)
 })
