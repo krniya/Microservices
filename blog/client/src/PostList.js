6ef40cc2cc40e5b945c65ddd123512d21607fb78
Nitish Kumar <krnitish@live.in>
2023-07-31 23:40:55 +0530

    Client updated with Post and Comments - Added post creation functionality - Added comments creation functionality - fixed warnings

---
+++ blog/client/src/PostList.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import CommentCreate from "./CommentCreate";
+
+const PostList = () => {
+    const [posts, setPosts] = useState({});
+    const fetchPosts = async () => {
+        const res = await axios.get("http://localhost:4000/posts");
+        setPosts(res.data);
+    };
+
+    useEffect(() => {
+        fetchPosts();
+    }, []);
+
+    const renderPosts = Object.values(posts).map((post) => {
+        return (
+            <div className="card" style={{ width: "30%", marginBottom: "20px" }} key={post.id}>
+                <div className="card-body">
+                    <h3>{post.title}</h3>
+                    <CommentCreate postId={post.id} />
+                </div>
+            </div>
+        );
+    });
+
+    return <div className="d-flex flex-row flex-wrap justify-content-between">{renderPosts}</div>;
+};
+
+export default PostList;
