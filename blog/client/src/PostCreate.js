a95303d53411ea0da4411f988eac0caac1d43887
Nitish Kumar <krnitish@live.in>
2023-11-09 20:43:24 +0530

    Skaffold configured - Added changes for post creation

--- blog/client/src/PostCreate.js
+++ blog/client/src/PostCreate.js
@@ -5,7 +5,7 @@ const PostCreate = () => {
     const [title, setTitle] = useState("");
     const onSubmit = async (event) => {
         event.preventDefault();
-        await axios.post("http://posts.com/posts", {
+        await axios.post("http://posts.com/posts/create", {
             title,
         });
         setTitle("");
